ded498fa6fc9d2e6bc0df81d6f71573cbb815436
MarcinJadczak <jadczak.martin@gmail.com>
2023-12-10 17:33:22 +0100

    zadanie skonczone!

--- js/task-04.js
+++ js/task-04.js
@@ -8,12 +8,11 @@ const liczba = document.querySelector("[data-create]");
 liczba.addEventListener("click", (event) => {
   event.preventDefault();
   const numer = document.querySelector("input").value;
-  console.log(numer);
   for (let i = 0; i < numer; i++) {
     const box = document.createElement("div");
     box.style.background = getRandomHexColor();
-    box.style.height = 30 + i * 10;
-    box.style.width = 30 + i * 10;
+    box.style.height = 30 + i * 10 +"px";
+    box.style.width = 30 + i * 10+ "px";
     let first = document.querySelector("#boxes");
     first.appendChild(box);
   }
@@ -21,54 +20,10 @@ liczba.addEventListener("click", (event) => {
 
 
 
+const del = document.querySelector('[data-destroy]');
+del.addEventListener("click",()=>{
+  let first = document.querySelector("#boxes");
+  first.innerHTML = "";
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const del = document.querySelector('[data-destroy]');
-// del.addEventListener("click",()=>{
-//   let first = document.querySelector("#boxes");
-//   const coUsuwamy= first.children;
-//   coUsuwamy.remove();
-// })
-
-// const number = document.querySelector('.number');
-// const createButtonEvent= document.querySelector('[data-create]');
-
-// let amount =0
-// createButtonEvent.addEventListener("click",() =>{
-//   amount = amount + number
-// });
-
-// createButtonEvent.addEventListener("click",() =>{
-//   const liczba = document.getElementsByTagName('input').value;
-//   console.log(liczba);
-// });
-
-// function createBoxes(amount){
-// for(let item of amount){
-// const box =document.createElement('div');
-// box.style.background = getRandomHexColor;
-// box.style.height += 10;
-// box.style.width += 10;
-// first = document.querySelector('.boxes');
-// first.appendChild(box);
-// }
-
-// }
